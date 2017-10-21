5fda3ffaa9bfdd54eda7522eb8097ba6b4f460e6
r3drighthand <ryanmccool13@gmail.com>
2017-10-20 19:32:45 -0500

    comment out index body

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,41 +3,43 @@ require 'aws-sdk'
 class PlaylistsController < ApplicationController
 
   def index
-    RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
-    # me = RSpotify::User.find('ryanmccool13')
+  #   RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+  #   # me = RSpotify::User.find('ryanmccool13')
 
-    # me.playlists.each do |playlist|
-    #   puts playlist.name
-    # end
-    # puts me.playlists[0].tracks.first.preview_url
-    # puts me.playlists[0].tracks.first.album.images
-    me = RSpotify::User.find('12122573728')
-    playlist = me.playlists.first
-    # puts playlist.tracks.count
-    # puts playlist.name
-    file = File.open("app/assets/mylist.txt", 'w') #{ |file| file.write("yourtext")}
-    playlist.tracks.each do |track|
-      if track.preview_url
-        file.puts("file " + track.preview_url.to_s)
-      end
-    end
-    file.close unless file.nil?
+  #   # me.playlists.each do |playlist|
+  #   #   puts playlist.name
+  #   # end
+  #   # puts me.playlists[0].tracks.first.preview_url
+  #   # puts me.playlists[0].tracks.first.album.images
+  #   me = RSpotify::User.find('12122573728')
+  #   playlist = me.playlists.first
+  #   # puts playlist.tracks.count
+  #   # puts playlist.name
+  #   file = File.open("app/assets/mylist.txt", 'w') #{ |file| file.write("yourtext")}
+  #   playlist.tracks.each do |track|
+  #     if track.preview_url
+  #       file.puts("file " + track.preview_url.to_s)
+  #     end
+  #   end
+  #   file.close unless file.nil?
 
-    system "ffmpeg -f concat -safe 0 -protocol_whitelist 'file,http,https,tcp,tls' -i app/assets/mylist.txt -c copy this-should-be-new.mp3"
-  #   system "ffmpeg -i testoutput5.mp3 -c:a aac -b:a 128k output.m4a"
-  #   system "ffmpeg -loop 1 -i img.jpg -i output.m4a -c:v libx264 -c:a copy -shortest out2.mp4"
+  #   system "ffmpeg -f concat -safe 0 -protocol_whitelist 'file,http,https,tcp,tls' -i app/assets/mylist.txt -c copy this-should-be-new.mp3"
+  # #   system "ffmpeg -i testoutput5.mp3 -c:a aac -b:a 128k output.m4a"
+  # #   system "ffmpeg -loop 1 -i img.jpg -i output.m4a -c:v libx264 -c:a copy -shortest out2.mp4"
 
-  #  file_name = 'output3.mp3'
-    file_name = 'this-should-be-new.mp3'
+  # #  file_name = 'output3.mp3'
+  #   file_name = 'this-should-be-new.mp3'
 
-    s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
-    obj = s3.bucket('dbc-team-samplify-test').object(file_name)
-    puts "Uploading file #{file_name}"
-    # / in front of app dir?
-    obj.upload_file("#{file_name}")
-    puts "Done"
+  #   s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+  #   obj = s3.bucket('dbc-team-samplify-test').object(file_name)
+  #   puts "Uploading file #{file_name}"
+  #   # / in front of app dir?
+  #   obj.upload_file("#{file_name}")
+  #   puts "Done"
 
   end
+
+###############
 
   # def show
   #   file_name = 'output3.mp3'

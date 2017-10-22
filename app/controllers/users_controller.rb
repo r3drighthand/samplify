709d1c3a915c46cb4c08c34e0a7efb0d1fa55ba2
Gilad Shanan <email": null>
2017-10-22 11:26:13 -0500

    update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
 
+  def index
+    #
+  end
+
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    user = User.find_or_create_by(display_name: spotify_user.display_name, email: spotify_user.email, spotify_id: spotify_user.id, profile_pic_url: spotify_user.images[0][:url])
+    p spotify_user.display_name
+    if spotify_user.display_name
+      display_name = spotify_user.display_name
+    else
+      display_name = spotify_user.email
+    end
+    # this should be a validation callback
+    if spotify_user.images.empty?
+      profile_pic_url = "https://img.buzzfeed.com/buzzfeed-static/static/2014-11/7/18/enhanced/webdr01/longform-original-18085-1415403731-23.jpg?crop=612:612;221,96&downsize=715:*&output-format=auto&output-quality=auto"
+    else
+      profile_pic_url = spotify_user.images[0][:url]
+    end
+    user = User.find_or_create_by(display_name: display_name, email: spotify_user.email, spotify_id: spotify_user.id, profile_pic_url: profile_pic_url)
     session[:user_id] = spotify_user.id
-    redirect_to root_path
+    redirect_to user_path(current_user)
   end
 
   def show

c2f9f58c9b09e2f85424a93bfb50ae4a44bffe54
Sam Tietze <samtietze@gmail.com>
2017-10-23 10:16:50 -0500

    Add log level to ffmpeg command

--- app/controllers/samplers_controller.rb
+++ app/controllers/samplers_controller.rb
@@ -117,7 +117,7 @@ class SamplersController < ApplicationController
     ########################################
     # This is the low-qual vid codec
     ########################################
-    system "ffmpeg -f concat -safe 0 -protocol_whitelist 'file,http,https,tcp,tls' -i tmp/#{@sampler.id}-images.txt -i tmp/keepItSimple.mp3 -c:a aac -b:a 128k -c:v mpeg4 tmp/#{@sampler.id}-sampler.mp4"
+    system "ffmpeg -loglevel 56 -y -f concat -safe 0 -protocol_whitelist 'file,http,https,tcp,tls' -i tmp/#{@sampler.id}-images.txt -i tmp/keepItSimple.mp3 -c:a aac -b:a 128k -c:v mpeg4 tmp/#{@sampler.id}-sampler.mp4"
     @file_name= "#{@sampler.id}-sampler.mp4"
     p '*************************************'
     p 'After video creation'

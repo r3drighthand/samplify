70b84aa63154249977b28246e67e56fde5ac7135
Brian Lin <lin.brian.l@gmail.com>
2017-10-23 22:06:27 -0500

    add thumbnails, track info, open in spotify button

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -6,12 +6,8 @@ $(document).ready(function() {
     var play_count = count
     var aud_control = event.target
     var image_index = 0
+    aud_control.volume = 0.35
     window.setInterval(function() {
-      if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 1 && play_count === 1) {
-        aud_control.volume = 0.35
-        console.log(aud_control.volume)
-        console.log("sec 1")
-      }
       if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 2 && play_count === 1) {
         aud_control.volume = 0.7
         console.log(aud_control.volume)
@@ -41,24 +37,9 @@ $(document).ready(function() {
     }, 1000)
     count += 1
 
-    // var slideIndex = 0;
-    // showSlides();
-
-    // function showSlides() {
-    //   var i;
-    //   var slides = document.getElementsByClassName("mySlides");
-    //   for (i = 0; i < slides.length; i++) {
-    //     slides[i].style.display = "none";
-    //   }
-    //   slideIndex++;
-    //   if (slideIndex > slides.length) {slideIndex = 1}
-    //   slides[slideIndex-1].style.display = "block";
-    //   setTimeout(showSlides, 30000); // Change image every 2 seconds
-    // }
   })
 
+// myaudio.currentTime = 0; - This will rewind the audio to the beginning.
 
-
-
-
+// If you want a function to be called once the audio has finished playing then you can use 'myaudio.addEventListener('ended',myfunc)' - This will call 'myfunc()' once the audio has finished.
 })

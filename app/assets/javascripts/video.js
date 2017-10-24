80227630352c74d0c4dac14ae7d354b727db1b8c
Brian Lin <lin.brian.l@gmail.com>
2017-10-23 22:45:45 -0500

    add dynamic volume fade in and out

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -7,28 +7,33 @@ $(document).ready(function() {
     var aud_control = event.target
     var image_index = 0
     aud_control.volume = 0.35
+    console.log("start: " + aud_control.volume)
     window.setInterval(function() {
+      if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 1 && play_count === 1) {
+        aud_control.volume /= 0.7
+        console.log("sec 1: " + aud_control.volume)
+      }
       if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 2 && play_count === 1) {
-        aud_control.volume = 0.7
-        console.log(aud_control.volume)
-        console.log("sec 2")
+        aud_control.volume /= 0.7
+        console.log("sec 2: " + aud_control.volume)
       }
       if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 3 && play_count === 1) {
-        aud_control.volume = 1.0
-        console.log(aud_control.volume)
-        console.log("sec 3")
+        aud_control.volume = 1
+        console.log("sec 3: " + aud_control.volume)
       }
       if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 28 && play_count === 1) {
-        aud_control.volume = 0.7
-        console.log(aud_control.volume)
-        console.log("sec 28")
+        aud_control.volume *= 0.7
+        console.log("sec 28: " + aud_control.volume)
       }
       if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 29 && play_count === 1) {
-        aud_control.volume = 0.35
-        console.log(aud_control.volume)
-        console.log("sec 29")
+        aud_control.volume *= 0.7
+        console.log("sec 29: " + aud_control.volume)
       }
     if ($("audio").get(0).paused === false && (Math.floor(aud_control.currentTime) % 30 ) === 0 && play_count === 1) {
+        if (aud_control.volume > 0.4) {
+          aud_control.volume *= 0.7
+        }
+        console.log("sec 0: " + aud_control.volume)
         image_index = Math.floor(aud_control.currentTime/30)
         $(".mySlides").hide()
         $(".mySlides").eq(image_index).attr("style", "display:block")

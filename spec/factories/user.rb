caf7007eb299450bc484af6159df20bb3ed1d0fa
Sam Tietze <samtietze@gmail.com>
2017-10-25 10:23:39 -0500

    Add user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-
+    display_name { Faker::StarTrek.character }
+    email { Faker::Internet.safe_email }
+    spotify_id { Faker::Internet.user_name(display_name) }
+    profile_pic_url { Faker::Avatar.image(spotify_id, "300x300", "png", "set4", "bg2") }
   end
 end

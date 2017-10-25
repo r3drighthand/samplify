55dc31a483c0f0bcbf0a1f3e21f83f4907f8b6b4
Brian Lin <lin.brian.l@gmail.com>
2017-10-25 16:39:56 -0500

    Toggle button presses but need to disable JS on other pages.

--- app/assets/javascripts/profile-buttons.js
+++ app/assets/javascripts/profile-buttons.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $(".playlist-button").on("click", function(event) {
     if ($(".page-content-right").is(":visible")) {
+      $(".playlist-button").toggleClass("pressed");
+      $(".sampler-button").toggleClass("pressed");
       $(".page-content-left").toggle();
       $(".page-content-right").toggle();
     };
   });
   $(".sampler-button").on("click", function(event) {
     if ($(".page-content-left").is(":visible")) {
+      $(".playlist-button").toggleClass("pressed");
+      $(".sampler-button").toggleClass("pressed");
       $(".page-content-left").toggle();
       $(".page-content-right").toggle();
     };

862e4802c1bfe8aed8b56804324ea107350f50e2
Brian Lin <lin.brian.l@gmail.com>
2017-10-25 16:10:28 -0500

    Toggle create and view samplers.

--- app/assets/javascripts/profile-buttons.js
+++ app/assets/javascripts/profile-buttons.js
@@ -1,1 +1,14 @@
-profile-buttons.js
+$(document).ready(function() {
+  $(".playlist-button").on("click", function(event) {
+    if ($(".page-content-right").is(":visible")) {
+      $(".page-content-left").toggle();
+      $(".page-content-right").toggle();
+    };
+  });
+  $(".sampler-button").on("click", function(event) {
+    if ($(".page-content-left").is(":visible")) {
+      $(".page-content-left").toggle();
+      $(".page-content-right").toggle();
+    };
+  });
+});

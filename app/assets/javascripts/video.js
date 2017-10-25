6c445e0793c8e10b59de0567d62950466a15d7d5
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2017-10-24 20:59:36 -0500

    Update video.js

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -1,8 +1,5 @@
 $(document).on('turbolinks:load', function() {
-    console.log(window.location.href)
-    var fader = 0;
   if (window.location.href.search(/(samplers\/\d+)/) > 0) {
-    console.log("show page!")
     $(".mySlides").hide()
     $(".mySlides").first().show()
     var audioController = $("audio")[0]
@@ -26,41 +23,31 @@ $(document).on('turbolinks:load', function() {
         audioController.pause();
       }
       var playCount = count
-      console.log("start: " + audioController.volume)
-      fader = setInterval(function() {
+      setInterval(function() {
         if ($("audio").get(0).paused === false && (Math.floor(audioController.currentTime) % 30 ) === 1 && playCount === 1) {
           audioController.volume /= 0.7
-          console.log("sec 1: " + audioController.volume)
         }
         if ($("audio").get(0).paused === false && (Math.floor(audioController.currentTime) % 30 ) === 2 && playCount === 1) {
           audioController.volume /= 0.7
-          console.log("sec 2: " + audioController.volume)
         }
         if ($("audio").get(0).paused === false && (Math.floor(audioController.currentTime) % 30 ) === 3 && playCount === 1) {
           audioController.volume = 1
-          console.log("sec 3: " + audioController.volume)
         }
         if ($("audio").get(0).paused === false && (Math.floor(audioController.currentTime) % 30 ) === 28 && playCount === 1) {
           audioController.volume *= 0.7
-          console.log("sec 28: " + audioController.volume)
         }
         if ($("audio").get(0).paused === false && (Math.floor(audioController.currentTime) % 30 ) === 29 && playCount === 1) {
           audioController.volume *= 0.7
-          console.log("sec 29: " + audioController.volume)
         }
         if ($("audio").get(0).paused === false && (Math.floor(audioController.currentTime) % 30 ) === 0 && playCount === 1) {
           if (audioController.volume > 0.4) {
             audioController.volume *= 0.7
           }
-          console.log("sec 0: " + audioController.volume)
           imageIndex = Math.floor(audioController.currentTime/30)
           $(".mySlides").hide()
           $(".mySlides").eq(imageIndex).attr("style", "display:block")
-          console.log(imageIndex)
         }
       }, 1000)
-    console.log(fader+" this is fader")
-
       count += 1
     })
     var downloadChecker = setInterval(function(){
@@ -76,13 +63,5 @@ $(document).on('turbolinks:load', function() {
         clearInterval(downloadChecker);
       });
     }, 5000)
-  } else {
-    console.log(fader)
-    clearInterval(fader)
   }
-  // $( window ).on ("beforeunload", function() {
-  //   console.log("leaving the page")
-  //   audioController.pause()
-  // });
-
 })

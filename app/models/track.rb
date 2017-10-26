d41c6a7c03ac424ec51ebbf2e3fa3186b67e4b58
Gilad Shanan <email": null>
2017-10-26 10:47:10 -0500

    start adjusting image size for sampler show page, add splash page syling

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ApplicationRecord
   belongs_to :sampler
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "980x980>", medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png"
 
   validates_presence_of :name, :sampler_id
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/

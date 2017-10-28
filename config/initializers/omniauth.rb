70d0a6b067a933ce04046cbc2a7dc91339f75808
Gilad Shanan <giladshanan@gmail.com>
2017-10-28 15:47:41 -0500

    remove more dialog-true relateed stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 require 'rspotify/oauth'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: "playlist-read-private user-follow-read user-read-email", show_dialog: "true"
+  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: "playlist-read-private user-follow-read user-read-email"
 end
 
 
